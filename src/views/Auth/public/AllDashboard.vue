c09e3cc02e0a18a025c97bb72418c9a47eac60ad
ADRIAN <adriandeligero.1030@gmail.com>
2026-02-11 09:47:44 +0800

    removed unused file

--- src/views/Auth/public/AllDashboard.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div class="dashboard-page">
-    <h1>Welcome to the Dashboard</h1>
-    <p>This is a public dashboard view.</p>
-  </div>
-</template>
