7db03165f4a1897e9f2e84ac8f04ce82d8ec6921
ADRIAN <adriandeligero.1030@gmail.com>
2026-03-02 09:27:39 +0800

    cleaned: no errors

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,17 +1,10 @@
-<script setup lang="ts">
-import { inject } from 'vue'
-import type { User } from '@/types/user'
-import type { Ref } from 'vue'
-
-const user = inject<Ref<User | null>>('user')
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div v-if="user">
+  <div>
     <h1 class="text-h4 font-weight-bold mb-4">Dashboard</h1>
     <v-card>
       <v-card-text>
-        <p class="text-h6">Welcome, {{ user.f_name }} {{ user.l_name }}!</p>
         <p class="text-body-1 mt-2">This is your dashboard overview.</p>
       </v-card-text>
     </v-card>
